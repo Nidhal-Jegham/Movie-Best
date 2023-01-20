753b305709ad8bba6d2c0ee3dfba8cdf6e19e39c
Yosri-zayani <99374166+Yosri-zayani@users.noreply.github.com>
2023-01-20 17:33:51 +0100

    Update and rename signup.html to signup.php

--- signup.php
+++ signup.php
@@ -2,6 +2,37 @@
 <html lang="en">
 
 <head>
+	<?php
+session_start();{
+    include("connection.php");
+    include ("functions.php");
+
+};
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    //something was posted 
+    $user_name =$_POST['user_name'];
+    $password =$_POST['password'];
+    $email = $_POST['email'];
+    if ( !empty($user_name) && !empty($password))
+    {
+        //save into database
+        $user_id = random_num(20);
+        $res = "insert into the_user (user_id,name,email,password) values ('$user_id','$user_name','$email','$password')";
+        mysqli_query($conn,$res);
+        header('location: index.php');
+        die;
+
+    }
+    else
+    {
+        echo "Enter valid info" ; 
+    }
+    
+
+}
+
+?>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
